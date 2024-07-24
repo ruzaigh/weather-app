c4a93f1b7436c4525c264b6fcf4241320a050f2a
Ruzaigh <ruzaigh.kalam2@gmail.com>
2024-07-24 10:55:54 +0200

    -created a api call to get the location of a place - created a api calll to get the current weather  of the place that a user selects

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  weatherApiKey: '1d75567878bf088b2e151bd8e0d33ade'
+}
---
+++ src/app/features/home/models/location.d.ts
@@ -0,0 +1,8 @@
+export interface Loaction{
+  country: string;
+  lat: number;
+  lon: number;
+  name: string;
+  state: string;
+  location_names: Object;
+}
